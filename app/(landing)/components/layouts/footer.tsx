0b690d0f47b913c775b6fede81ce1e1deab68864
MyrasYa <rasyaakbar2310@gmail.com>
2026-01-27 12:34:49 +0700

    Fix Footer

--- app/(landing)/components/layouts/footer.tsx
+++ app/(landing)/components/layouts/footer.tsx
@@ -12,14 +12,14 @@ const Footer = () => {
             width={167}
             height={44}
           />
-          <p className="mt-8 max-w-[420px] leading-relaxed text-sm sm:text-base">
+          <p className="mt-8 max-w-[420px] leading-relaxed text-sm sm:base]">
             Engineered for endurance and designed for speed. Experience gear
             that moves as fast as you do.
           </p>
         </div>
 
-        <div className="grid grid-cols-2 pt-15 w-full sm:w-105">
-          <div className="flex gap-7 flex-col">
+        <div className="sm:grid grid-cols-2 pt-15 w-full sm:w-105 sm:text-base grid-cols-1">
+          <div className="flex gap-7 flex-col mb-8">
             <Link href="#">Home</Link>
             <Link href="#">Categories</Link>
             <Link href="#">Products</Link>
@@ -34,10 +34,10 @@ const Footer = () => {
         </div>
       </div>
       <div className="border-t border-t-white/15">
-        <div className="container mx-auto py-6.5 flex justify-between ">
-          <div className="mr-5">SportsOn © 2025 All Rights Reserverd.</div>
-          <div className="grid grid-cols-2 w-full sm:w-105">
-            <Link href="#" className="mx-2">Privacy Policy</Link>
+        <div className="container mx-auto py-6.5  flex justify-between text=base ">
+          <div className="mr-8">SportsOn © 2025 All Rights Reserverd.</div>
+          <div className="grid grid-cols-2 w-full sm:w-105 ">
+            <Link href="#" className="mr-2 ">Privacy Policy</Link>
             <Link href="#" className="mr-4">Terms Conditions</Link>
           </div>
         </div>
